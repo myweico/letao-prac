23017bd9a60f91ef74072bda3d53261355b52065
myweico <470398956@qq.com>
2018-10-05 15:21:11 +0800

    complete search page

--- public/mine/js/search.js
+++ public/mine/js/search.js
@@ -1,137 +1,100 @@
 $(function () {
-  /* 全局变量 */
-  var options = {
-    page: 0,
-    proName: ''
-  }
-  // 为了判断是否还有数据，page固定
-  var pageSize = 4
+  // 获取存储的history
+  var history = JSON.parse(localStorage.getItem('history')) || []
 
-  // 初始化区域滚动
-  mui('.mui-scroll-wrapper').scroll({
-    scrollY: true, //是否竖向滚动
-    scrollX: false, //是否横向滚动
-    startX: 0, //初始化时滚动至x
-    startY: 0, //初始化时滚动至y
-    indicators: false, //是否显示滚动条
-    deceleration: 0.001, //阻尼系数,系数越小滑动越灵敏
-    bounce: true //是否启用回弹
-  })
+  // 显示历史纪录
+  showHistory(history)
 
-  // 初始化下拉刷新
-  mui.init({
-    pullRefresh: {
-      container: "#refreshContainer", //下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
-      down: {
-        height: 50, //可选,默认50.触发下拉刷新拖动距离,
-        auto: false, //可选,默认false.首次加载自动下拉刷新一次
-        contentdown: "下拉可以刷新", //可选，在下拉可刷新状态时，下拉刷新控件上显示的标题内容
-        contentover: "释放立即刷新", //可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
-        contentrefresh: "正在刷新...", //可选，正在刷新状态时，下拉刷新控件上显示的标题内容
-        callback() {
-          //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-          // name,price,num保持不变
-          // page重置为0
-          options.page = 1
-          getSearch(options, (data) => {
-            setTimeout(() => {
-              mui('#refreshContainer').pullRefresh().endPulldownToRefresh()
-              mui('#refreshContainer').pullRefresh().refresh(true)
-              renderList(data)
-            }, 1000)
-            // mui('#refreshContainer').pullRefresh().endPullDown() //官网的方法不存在？？
-          })
-        }
-      },
-      up: {
-        height: 50, //可选.默认50.触发上拉加载拖动距离
-        auto: false, //可选,默认false.自动上拉加载一次
-        contentrefresh: "正在加载...", //可选，正在加载状态时，上拉加载控件上显示的标题内容
-        contentnomore: '没有更多数据了', //可选，请求完毕若没有更多数据时显示的提醒内容；
-        callback() {
-          options.page = parseInt(options.page) + 1
-          getSearch(options, (data) => {
-            setTimeout(() => {
-              let hasNoMore = options.page * pageSize > parseInt(data.count)
-              if (hasNoMore) {
-                // 回退有数据的页面
-                options.page = options.page - 1
-                mui('#refreshContainer').pullRefresh().endPullupToRefresh(true)
-              } else {
-                mui('#refreshContainer').pullRefresh().endPullupToRefresh()
-              }
-              let renderStr = template('search-result', data)
-              $('.product-list').append($(renderStr))
-            }, 1000)
-          })
+  // 点击按钮，加入搜索历史
+  addHistory(history)
 
-        } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-      }
-    }
-  })
+  // 清空功能
+  bindClearHistory()
 
-  // 绑定搜索事件
+  // 点击纪录跳转
+  bindLink(history)
+
+  // 删除单个纪录
+  bindDeleteHistory(history)
+})
+
+function addHistory(history) {
   $('.search-input button').on('tap', function () {
-    let $this = $(this)
+    let keyword = $(this).prev().val().trim()
 
-    // 重置请求参数
-    options = {
-      proName: $this.prev().val().trim(),
-      page: 1
-    }
-
-    // 若为空，提示输入关键字
-    if (options.proName.length === 0) {
+    // 1.1 若为空，则提示输入关键字
+    if (keyword.length === 0) {
       mui.toast('请输入关键字')
       return
     }
 
-    getSearch(options, renderList)
-  })
-
-  // 绑定排序事件
-  $('.orderbar>span').on('tap', function () {
-    let $this = $(this)
-    let $icon = $this.children('span')
-    let order = $this.data('order')
-    if (!$this.hasClass('selected')) {
-      $this.addClass('selected').siblings().removeClass('selected')
-      $('.orderbar span[class~=mui-icon]').removeClass('mui-icon-arrowup').addClass('mui-icon-arrowdown')
-
-      options.price = 2
-      options.num = 2
-    } else if ($icon.hasClass('mui-icon-arrowdown')) {
-      $icon.removeClass('mui-icon-arrowdown').addClass('mui-icon-arrowup')
-      options[order] = 1
-    } else {
-      $icon.removeClass('mui-icon-arrowup').addClass('mui-icon-arrowdown')
-      options[order] = 2
+    // 1.2 判断关键字是否存在，存在则删除原记录
+    if (history.includes(keyword)) {
+      let delIndex = history.findIndex((item) => {
+        return item === keyword
+      })
+      history.splice(delIndex, 1)
     }
-    // 重新获取列表
-    options.page = 1
-    mui('#refreshContainer').pullRefresh().refresh(true)
-    getSearch(options, renderList)
+
+    // 1.3 加入到新的记录
+    history.push(keyword)
+
+    // 1.4 当记录超过10条的时候就覆盖最旧的历史记录
+    if (history.length > 10) {
+      history.splice(0, history.length - 10)
+    }
+
+    // 1.5 保存到storage中
+    localStorage.setItem('history', JSON.stringify(history))
+
+    // 1.6 跳转到搜索结果页面
+    location.href = 'searchResult.html?keyword=' + keyword
   })
+}
 
-  function getSearch(optoins, callback) {
-    $.ajax({
-      url: '/product/queryProduct',
-      data: {
-        proName: options.proName || '',
-        page: options.page || 1,
-        pageSize: pageSize || 4,
-        brandId: options.brandId || '',
-        price: options.price || '',
-        num: options.num || ''
-      },
-      success(data) {
-        callback && callback(data)
-      }
+function showHistory(history) {
+  let renderStr = template('historyTemp', {
+    history: history
+  })
+  $('.history').html(renderStr)
+}
+
+function bindClearHistory(history) {
+  $('.history').on('tap', '.clearHistory', function () {
+    localStorage.removeItem('history')
+    // 重新渲染
+    let renderStr = template('historyTemp', {
+      history: []
     })
-  }
+    $('.history').html(renderStr)
+  })
+}
 
-  function renderList(data) {
-    let str = template('search-result', data)
-    $('.product-list').html(str)
-  }
-})
+function bindLink(history) {
+  $('.history').on('tap', '.history-list a', function () {
+    let keyword = $(this).text()
+    let delIndex = history.findIndex((item) => {
+      return item === keyword
+    })
+    history.splice(delIndex, 1)
+    history.push(keyword)
+    localStorage.setItem('history', JSON.stringify(history))
+    location.href = 'searchResult.html?keyword=' + keyword
+  })
+}
+
+function bindDeleteHistory(history) {
+  $('.history').on('tap', '.history-list span', function () {
+    let item = $(this).prev().text()
+    let delIndex = history.findIndex((value) => {
+      return value === item
+    })
+    history.splice(delIndex, 1)
+    localStorage.setItem('history', JSON.stringify(history))
+    // 重新渲染
+    let renderStr = template('historyTemp', {
+      history
+    })
+    $('.history').html(renderStr)
+  })
+}
