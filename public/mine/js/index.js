d456b781accfdd72acf854bec0d44a8103d7801b
myweico <470398956@qq.com>
2018-10-06 21:13:36 +0800

    add the link on homepage

--- public/mine/js/index.js
+++ public/mine/js/index.js
@@ -2,7 +2,7 @@ $(function () {
   // 初始化轮播图
   var gallery = mui('.mui-slider')
   gallery.slider({
-    interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+    interval: 2500 //自动轮播周期，若为0则不自动播放，默认为0；
   })
 
   // 初始化区域滚动
@@ -14,5 +14,11 @@ $(function () {
    indicators: false, //是否显示滚动条
    deceleration: 0.001, //阻尼系数,系数越小滑动越灵敏
    bounce: true //是否启用回弹
-  });
+  })
+
+  // 初始化立即购买
+  $('.button-buy').on('tap', function () {
+    console.log('buy buy buy!!!')
+    location.href = "/mine/product.html?productId=1"
+  })
 })
