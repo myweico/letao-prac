be46737385e53e9f162d7063098fe6d1ca359788
myweico <470398956@qq.com>
2018-10-11 15:16:12 +0800

    add confirm box when logout

--- public/mine/js/profile.js
+++ public/mine/js/profile.js
@@ -3,14 +3,18 @@ $(function () {
   
   // 绑定登出操作
   $('.loginout').on('tap', function () {
-    $.ajax({
-      url: '/user/logout',
-      success: function (data) {
-        if (data.success) {
-          location.href = '/mine/'
-        } else {
-          mui.toast('服务器繁忙，请稍后再试')
-        }
+    mui.confirm('退出登录？', '', ['确定', '取消'], function (e) {
+      if (e.index === 0) {
+        $.ajax({
+          url: '/user/logout',
+          success: function (data) {
+            if (data.success) {
+              location.href = '/mine/'
+            } else {
+              mui.toast('服务器繁忙，请稍后再试')
+            }
+          }
+        })
       }
     })
   })
